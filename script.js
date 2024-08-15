b014bc31beb332077c7c2d69abe17f6c4d821a1f
Goose-95 <147353241+Goose-95@users.noreply.github.com>
2024-08-15 14:11:38 +0900

    Wrote scrit to play one round of rock paper scissors. Renamed variable on line 21.

--- script.js
+++ script.js
@@ -18,16 +18,47 @@ function getComputerChoice (randomNumber) {
 
 console.log (getComputerChoice(randomNumber))
 
-let humanChoice = prompt('rock, paper, or scissors?')
+let humanInput = prompt('rock, paper, or scissors?')
 
-function getHumanChoice (humanChoice) {
-    if (humanChoice == 'rock') {
+function getHumanChoice (humanInput) {
+    if (humanInput == 'rock') {
         return 'rock'
-    } else if (humanChoice == 'paper') {
+    } else if (humanInput == 'paper') {
         return 'paper'
-    } else if (humanChoice == 'scissors') {
+    } else if (humanInput == 'scissors') {
         return 'scissors'
     } else alert('Please chose only rock, paper, or scissors. Reload and try again. Game is case sensitive!')
 }
 
-console.log (getHumanChoice(humanChoice))
+console.log (getHumanChoice(humanInput))
+
+let humanChoice = getHumanChoice (humanInput)
+
+let computerChoice = getComputerChoice (randomNumber)
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound (humanChoice, computerChoice) {
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        return 'win!'
+    } else if (humanChoice == 'rock' && computerChoice == 'rock') {
+        return 'tie!'
+    } else   if (humanChoice == 'rock' && computerChoice == 'paper') {
+        return 'lose!'
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        return 'win!'
+    } else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
+        return 'tie!'
+    } else   if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        return 'lose!'
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        return 'win!'
+    } else if (humanChoice == 'paper' && computerChoice == 'paper') {
+        return 'tie!'
+    } else   if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        return 'lose!'
+    }
+}
+
+console.log (playRound(humanChoice, computerChoice))
