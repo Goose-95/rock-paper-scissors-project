a8470c88402ef610de43503e6ca283267aee5a61
Goose-95 <147353241+Goose-95@users.noreply.github.com>
2024-08-20 14:49:12 +0900

    Added UI elements to the game, it is no longer played with console. Added nodes for UI elements. Added click even listener to run game scripts and nested functions inside. Click event runs game and displays/updates scores. Removed all console logs.

--- script.js
+++ script.js
@@ -4,59 +4,82 @@
 //write logic to play one round
 //write logic to play entire game
 
-let randomNumber = Math.random()
 
-function getComputerChoice (randomNumber) {
-    if (randomNumber >= .67) {
-        return 'rock'
-    } else if (randomNumber < .67 && randomNumber >= .33) {
-        return 'scissors'
-    } else {
-        return 'paper'
-    }
-}
 
-function playGame() {
-    while (humanScore <5 && computerScore <5) {
-        let humanChoice = prompt ('rock, paper, or scissors?')
-        let computerChoice = getComputerChoice(randomNumber)
-        alert(playRound(humanChoice, computerChoice))
-    }
-}
+
+let humanChoice = ''
+let computerChoice = ''
+ 
 
 let humanScore = 0
 let computerScore = 0
 
-    function playRound (humanChoice, computerChoice) {
-        if (humanChoice == 'rock' && computerChoice == 'scissors') {
-            humanScore += 1;
-            return 'win!'
-        } else if (humanChoice == 'rock' && computerChoice == 'rock') {
-            return 'tie!'
-        } else   if (humanChoice == 'rock' && computerChoice == 'paper') {
-            computerScore += 1;
-            return 'lose!'
-        } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-            humanScore += 1; 
-            return 'win!'
-        } else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
-            return 'tie!'
-        } else   if (humanChoice == 'scissors' && computerChoice == 'rock') {
-            computerScore += 1;
-            return 'lose!'
-        } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-            humanScore += 1;
-            return 'win!'
-        } else if (humanChoice == 'paper' && computerChoice == 'paper') {
-            return 'tie!'
-        } else   if (humanChoice == 'paper' && computerChoice == 'scissors') {
-            computerScore += 1;
-            return 'lose!'
-        }else {
-            return 'please enter a valid choice'
-        }
-    }
+const container = document.querySelector('#container')
+const buttons = document.querySelectorAll("button");
 
-    console.log(playGame())
-    console.log (humanScore)
-    console.log(computerScore)
+let roundResults = document.createElement ('div')
+container.appendChild(roundResults)
+let humanScoreResults = document.createElement ('div')
+container.appendChild(humanScoreResults)
+let computerScoreResults = document.createElement('div')
+container.appendChild(computerScoreResults)
+let gameResults = document.createElement('h1')
+container.appendChild(gameResults)
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanChoice = button.id;
+    let randomNumber = [Math.random()];
+
+    function getComputerChoice () {
+    
+        if (randomNumber >= .67) {
+        return 'rock'
+        } else if (randomNumber < .67 && randomNumber >= .33) {
+        return 'scissors'
+        } else {
+         return 'paper'
+        };
+    }
+        computerChoice = getComputerChoice();
+
+        function playRound (humanChoice, computerChoice) {
+            if (humanChoice == 'rock' && computerChoice == 'scissors') {
+                humanScore += 1;
+                return 'win!'
+            } else if (humanChoice == 'rock' && computerChoice == 'rock') {
+                return 'tie!'
+            } else   if (humanChoice == 'rock' && computerChoice == 'paper') {
+                computerScore += 1;
+                return 'lose!'
+            } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+                humanScore += 1; 
+                return 'win!'
+            } else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
+                return 'tie!'
+            } else   if (humanChoice == 'scissors' && computerChoice == 'rock') {
+                computerScore += 1;
+                return 'lose!'
+            } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+                humanScore += 1;
+                return 'win!'
+            } else if (humanChoice == 'paper' && computerChoice == 'paper') {
+                return 'tie!'
+            } else   if (humanChoice == 'paper' && computerChoice == 'scissors') {
+                computerScore += 1;
+                return 'lose!'
+            }else {
+                return 'please enter a valid choice'
+            }
+        }
+
+        roundResults.textContent = playRound(humanChoice,computerChoice);
+        humanScoreResults.textContent = "Your score: " + humanScore;
+        computerScoreResults.textContent = "Computer's score: " + computerScore;
+        if (humanScore >= 5) {
+            return gameResults.textContent = "you win!"
+        }else if (computerScore >= 5) {
+            return gameResults.textContent = "you lose!"
+        }else {return gameResults.textContent = ""}
+    })
+});
